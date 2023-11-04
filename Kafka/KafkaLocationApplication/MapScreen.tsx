6febf9b5fab9fad6d17fe386df69c62d1b702f88
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-11-04 14:12:29 -0500

    🗾 Geolocation improved with trails and another library

--- Kafka/KafkaLocationApplication/MapScreen.tsx
+++ Kafka/KafkaLocationApplication/MapScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, PermissionsAndroid, Platform } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import Geolocation from 'react-native-geolocation-service';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 function MapScreen() {
   const [location, setLocation] = useState(null);
-  const [locationPermissionGranted, setLocationPermissionGranted] = useState(null);
+  const [trail, setTrail] = useState([]);
+  const [locationPermissionGranted, setLocationPermissionGranted] = useState(false);
 
   // Check for permission and request if not granted
   const requestLocationPermission = async () => {
@@ -19,21 +20,12 @@ function MapScreen() {
           {
             title: "Location Access Required",
             message: "This app needs to access your location.",
-            buttonPositive: 'Test'
+            buttonPositive: "OK"
           }
         );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          // Permission granted
-          console.log("Location permission granted");
-          setLocationPermissionGranted(true);
-        } else {
-          // Permission denied
-          console.log("Location permission denied");
-          setLocationPermissionGranted(false);
-        }
+        setLocationPermissionGranted(granted === PermissionsAndroid.RESULTS.GRANTED);
       } catch (err) {
         console.warn(err);
-        console.log(err);
       }
     }
   };
@@ -43,17 +35,30 @@ function MapScreen() {
   }, []);
 
   useEffect(() => {
+    let watchId = null;
+
     if (locationPermissionGranted) {
-      Geolocation.getCurrentPosition(
+      // Update location every 500ms
+      watchId = Geolocation.watchPosition(
         (position) => {
-          setLocation(position.coords);
+          const { latitude, longitude } = position.coords;
+          setLocation({ latitude, longitude });
+          // Update the trail state to include the new location
+          setTrail((prevTrail) => [...prevTrail, { latitude, longitude }]);
         },
         (error) => {
           console.log(error.code, error.message);
         },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        { enableHighAccuracy: true, distanceFilter: 0, interval: 500 }
       );
     }
+
+    // Clear watch on unmount
+    return () => {
+      if (watchId !== null) {
+        Geolocation.clearWatch(watchId);
+      }
+    };
   }, [locationPermissionGranted]);
 
   return (
@@ -61,12 +66,14 @@ function MapScreen() {
       {location ? (
         <MapView
           style={{ flex: 1 }}
-          initialRegion={{
+          region={{
             latitude: location.latitude,
             longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
           }}>
+          {/* Render the trail as a polyline */}
+          <Polyline coordinates={trail} strokeColor="#000" strokeWidth={3} />
           <Marker
             coordinate={{
               latitude: location.latitude,
@@ -76,7 +83,7 @@ function MapScreen() {
           />
         </MapView>
       ) : (
-        <Text>Getting the location...</Text>
+        <Text>{locationPermissionGranted ? 'Getting the location...' : 'Location permission not granted'}</Text>
       )}
     </View>
   );
