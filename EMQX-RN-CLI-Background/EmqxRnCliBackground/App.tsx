5b6c61bab33a573fa64e03eb58f986656c6fb37a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-12-09 20:22:13 -0500

    🔙 react-native-background-fetch background support added to the project

--- EMQX-RN-CLI-Background/EmqxRnCliBackground/App.tsx
+++ EMQX-RN-CLI-Background/EmqxRnCliBackground/App.tsx
@@ -1,51 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StatusBar, Text, useColorScheme } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
-import { Client, Message } from 'react-native-paho-mqtt';
+import {Client, Message} from 'react-native-paho-mqtt';
 
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const [location, setLocation] = useState({latitude: 0, longitude: 0});
 
   // Set up an in-memory alternative to global localStorage
   const myStorage = {
     setItem: (key, item) => {
       myStorage[key] = item;
     },
-    getItem: (key) => myStorage[key],
-    removeItem: (key) => {
+    getItem: key => myStorage[key],
+    removeItem: key => {
       delete myStorage[key];
     },
   };
 
   // Create a client instance
   const client = new Client({
-    uri: 'ws://192.168.20.33:8083/mqtt', // Replace with your EMQX WebSocket URL
+    uri: 'ws://192.168.20.33:8083/mqtt',
     clientId: 'yourClientId',
-    storage: myStorage
+    storage: myStorage,
   });
 
   // Connect the client and set event handlers
   useEffect(() => {
-    client.connect()
+    client
+      .connect()
       .then(() => {
         console.log('Connected to MQTT Broker!');
       })
-      .catch((error) => {
+      .catch(error => {
         console.log('Connection failed: ', error);
       });
 
     const watchId = Geolocation.watchPosition(
       position => {
-        const { latitude, longitude } = position.coords;
-        setLocation({ latitude, longitude });
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
 
-        const locationMessage = new Message(JSON.stringify({ latitude, longitude }));
+        const locationMessage = new Message(
+          JSON.stringify({latitude, longitude}),
+        );
         locationMessage.destinationName = 'locationTopic';
         client.send(locationMessage);
       },
       error => console.log(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 1 },
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 1,
+      },
     );
 
     return () => {
